0ed72d5779b24d1a3442646d15063959fd0b8004
bhaveshshrimali <bhavesh.shrimali@gmail.com>
2019-11-06 13:08:16 -0600

    add HW7 demo

--- demoHW6.py
+++ demoHW6.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 import numpy.linalg as nla
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_bvp, ode, odeint, solve_ivp
-from scipy.optimize import least_squares
+from scipy.optimize import least_squares, newton_krylov, anderson, broyden1
 plt.rc('text',usetex=True)
 plt.rc('font',**{'family':'serif','serif':['lmodern']})
 plt.rc('text',usetex=True)
@@ -82,7 +82,7 @@ def ode_sys(r, z, params):
     fpp_r = (-2.0*r**4*kappa*f_r**4*fp_r**4 - 8.0*r**3*kappa*f_r**5*fp_r**3 - 10.0*r**2*kappa*f_r**6*fp_r**2 + 2.0*r**2*mu*(r*fp_r + f_r)**alpha*fp_r**2 - 2.0*r**2*mu*fp_r**2 - 2*r*alpha*mu*(r*fp_r + f_r)**alpha*f_r*fp_r - 4.0*r*kappa*f_r**7*fp_r + 2.0*r*mu*(r*fp_r + f_r)**alpha*f_r*fp_r - 4.0*r*mu*f_r*fp_r + 2.0*r*mu*f_r**(alpha + 1)*fp_r - 2.0*mu*(r*fp_r + f_r)**alpha*f_r**2 + 2.0*mu*f_r**(alpha + 2))/(r**2*(r**2*kappa*f_r**4*fp_r**2 + 2.0*r*kappa*f_r**5*fp_r + alpha*mu*(r*fp_r + f_r)**alpha + kappa*f_r**6 - mu*(r*fp_r + f_r)**alpha + mu)*f_r)
     return [fp_r, fpp_r]
 
-def integrate_ode_sys(z_at_A, integrator, params, step=1.e-3, silent=True):
+def integrate_ode_sys(z_at_A, integrator, params, step=5.e-4, silent=True):
     """
     Calls an ODE integrator to integrate the initial value problem over the 
     shell thickness; starting from the outer surface
@@ -93,7 +93,7 @@ def integrate_ode_sys(z_at_A, integrator, params, step=1.e-3, silent=True):
     integrator.set_f_params(params)
     dt =step
     xs, zs = [], []
-    while integrator.successful() and integrator.t < B:
+    while integrator.successful() and integrator.t <= B:
         integrator.integrate(integrator.t + dt)
         xs.append(integrator.t)
         zs.append([integrator.y[0], integrator.y[1]])
@@ -101,7 +101,7 @@ def integrate_ode_sys(z_at_A, integrator, params, step=1.e-3, silent=True):
             print("Current x and z values: ", integrator.t, integrator.y)
     return xs, zs
 
-def solve_bvp_balloon(f_at_A, params, pvals, step=1.e-3, silent=True):
+def solve_bvp_balloon(f_at_A, params, pvals, step=5.e-4, silent=True):
     """
     Solve the boundary value problem by minimizing the loss function for 
     the mixed (Robin)-like BC at the inner boundary 
@@ -111,6 +111,7 @@ def solve_bvp_balloon(f_at_A, params, pvals, step=1.e-3, silent=True):
     mu, kapp, alph, A, B = params
     # xs, zs = [], []
     def residual_at_A(z_at_A):
+        # z_at_A = z_at_A[0]
         xs, zs = integrate_ode_sys(z_at_A, integrator, params)
         f_A, fp_A = np.array(zs)[0]
         f_B, fp_B = np.array(zs)[-1]
@@ -119,11 +120,12 @@ def solve_bvp_balloon(f_at_A, params, pvals, step=1.e-3, silent=True):
         return [bc_inside, bc_outside]
     
     fp_at_A = lambda fpEval: pvals + t1_func(A*fpEval, f_at_A, f_at_A)
-    fp_at_A_guess = least_squares(fp_at_A, 1./(1+pvals**(0.25)), method='trf', loss='soft_l1', max_nfev=3000)
+    fp_at_A_guess = least_squares(fp_at_A, 1./(1+pvals**(0.25)), method='trf', loss='soft_l1')
     z_at_A_guess = np.array([f_at_A, fp_at_A_guess.x])
     # z_at_B_guess = np.array([1., 0.01])
-    soln = least_squares(residual_at_A, z_at_A_guess, method='trf', loss='soft_l1', max_nfev=3000)
-    return soln.x 
+    # soln = least_squares(residual_at_A, z_at_A_guess, method='trf', loss='soft_l1', max_nfev=10000)
+    soln = least_squares(residual_at_A, z_at_A_guess, loss='soft_l1').x
+    return soln
 
 pressure_inside = np.linspace(0,0.09*10**6,501)
 A, B = 0.95, 1.
